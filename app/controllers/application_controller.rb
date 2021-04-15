99decd0a5bbb2ee8da7608950e22dae933baf054
Jesús Díaz Artiaga <jesus.diaz@howler.events>
2021-04-15 17:25:59 +0200

    chore: redirect to administrator page on login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,14 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
   end
+
+  def after_sign_in_path_for(user)
+    if user.type == 'Administrator'
+      administrator_path user
+    else
+      root_path
+    end
+  end
+
+
 end
