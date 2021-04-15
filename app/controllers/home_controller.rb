262b4f08a200698c7115f4b9587c998af92deb24
Jesús Díaz Artiaga <jesus.diaz@howler.events>
2021-04-15 17:41:21 +0200

    chore: redirect main home to administrator's home if logged ni

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
   def index
+    if current_user
+      if current_user.type == 'Administrator'
+        redirect_to administrator_path current_user
+      end
+    end
   end
 end
