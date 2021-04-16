82441e4eaffefc3d5f408d0d1c06038fab452386
Jesús Díaz Artiaga <jesus.diaz@howler.events>
2021-04-16 11:59:17 +0200

    chore: fill home screen for administrator

--- app/controllers/administrators_controller.rb
+++ app/controllers/administrators_controller.rb
@@ -2,6 +2,8 @@ class AdministratorsController < ApplicationController
 
   def show
     @administrator = Administrator.find(params[:id])
+    @teachers = Teacher.where(approved: true)
+    @pending_approvals = User.where(approved: false).sort_by{ |user| user.type }
   end
 
 end
