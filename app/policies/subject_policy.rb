70daa50e695b25bb622764d1d607224954be99bf
Jesús Díaz Artiaga <jesus.diaz@howler.events>
2021-05-05 19:57:59 +0200

    chore: update policies for subject

--- app/policies/subject_policy.rb
+++ app/policies/subject_policy.rb
@@ -1,12 +1,26 @@
 class SubjectPolicy < ApplicationPolicy
   include Roles
+  attr_reader :user, :subject
+
+  def initialize(user, subject)
+    @user = user
+    @subject = subject
+  end
 
   def index?
     true
   end
 
   def show?
-    true
+    if is_admin? user
+      true
+    elsif is_teacher? user
+      Seminar.where(subject: @subject).pluck(:teacher_id).include? user.id
+    elsif is_student? user
+      Attendee.where(subject: @subject).pluck(:student_id).include? user.id
+    else 
+      false
+    end
   end
 
   def create?
