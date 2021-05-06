3cd879bd78fb67e776d6ffc93f008269e1de6c91
Jesús Díaz Artiaga <jesus.diaz@howler.events>
2021-05-06 11:29:56 +0200

    chore: add Test model

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -3,4 +3,5 @@ class Subject < ApplicationRecord
   has_many :teachers, through: :seminars, source: :user
   has_many :attendees, dependent: :destroy
   has_many :students, through: :attendees, source: :user
+  has_many :tests
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_05_170428) do
+ActiveRecord::Schema.define(version: 2021_05_06_092803) do
 
   create_table "attendees", force: :cascade do |t|
     t.integer "subject_id", null: false
@@ -35,6 +35,14 @@ ActiveRecord::Schema.define(version: 2021_05_05_170428) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "tests", force: :cascade do |t|
+    t.string "name"
+    t.integer "subject_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["subject_id"], name: "index_tests_on_subject_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "first_name", default: "", null: false
     t.string "last_name", default: "", null: false
@@ -53,4 +61,5 @@ ActiveRecord::Schema.define(version: 2021_05_05_170428) do
 
   add_foreign_key "attendees", "subjects"
   add_foreign_key "seminars", "subjects"
+  add_foreign_key "tests", "subjects"
 end
