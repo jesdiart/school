972726891cba194e6b7b762b34f6c6fd89df7987
Jesús Díaz Artiaga <jesus.diaz@howler.events>
2021-05-06 11:08:58 +0200

    chore: update policies for seminar and attendee

--- app/policies/attendee_policy.rb
+++ app/policies/attendee_policy.rb
@@ -1,14 +1,6 @@
 class AttendeePolicy < ApplicationPolicy
   include Roles
 
-  def index?
-    true
-  end
-
-  def show?
-    true
-  end
-
   def create?
     is_teacher? user
   end
--- app/policies/seminar_policy.rb
+++ app/policies/seminar_policy.rb
@@ -1,14 +1,6 @@
 class SeminarPolicy < ApplicationPolicy
   include Roles
 
-  def index?
-    true
-  end
-
-  def show?
-    true
-  end
-
   def create?
     is_admin? user
   end
