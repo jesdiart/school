dd0ef717015ae0cdd71afb68027884e0bab4b9c9
Jesús Díaz Artiaga <jesus.diaz@howler.events>
2021-05-10 12:54:17 +0200

    chore: display Teachers' performance in Administrator screen

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,3 +1,12 @@
 class Teacher < User
   has_many :seminars
+  has_many :subjects, through: :seminars
+  has_many :students, through: :subjects
+
+  def students_performance
+    unless students.empty?
+      students.sum(&:performance_subjects) / students.count
+    end
+  end
 end
+
