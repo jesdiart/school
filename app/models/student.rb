7f3c34d175218052f8538ab3d4bfad96a11ba30c
Jesús Díaz Artiaga <jesus.diaz@howler.events>
2021-05-10 12:54:06 +0200

    chore: display students' performance in Teachers screen

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,20 @@ class Student < User
   has_many :attendees
   has_many :subjects, through: :attendees
   has_many :results
+
+  def performance_subject(subject)
+    subject_results = results.where(test: subject.tests)
+    performance_tests subject_results
+  end
+
+  def performance_subjects
+    performance_tests results
+  end
+  
+  def performance_tests(test_results)
+    unless test_results.empty?
+      test_results.sum(&:mark) / test_results.count
+    end
+  end
+
 end
